c6f7a040bb39e778dfe20e69bdd79982a887c6be
RAEnomentsoa <arakotomalala46@gmail.com>
2025-11-23 09:55:59 +0300

    sprint 4
    sprint 4 test get_return_type

--- test-app/out/TestController.java
+++ test-app/out/TestController.java
@@ -7,11 +7,12 @@ import core.annotation.Route;
 public class TestController {
 
     @Route("/test")
-    public String test(String path, String methodName, String ControllerName) {
-        return "<h1>Controller Test</h1>"
-                + "<p>URL: " + path + "</p>"
-                + "<p>Method: " + methodName + "</p>"
-                + "<p>Controller: " + ControllerName + "</p>";
+    public void test(String path, String methodName, String ControllerName, String returnTypeName) {
+        // return "<h1>Controller Test</h1>"
+        // + "<p>URL: " + path + "</p>"
+        // + "<p>Method: " + methodName + "</p>"
+        // + "<p>Controller: " + ControllerName + "</p>"
+        // + "<p>Return Type: " + returnTypeName + "</p>";
     }
 
     @Route("/test/hello")
