fc3ebf89e099c8f688b2d57c7b8e41cdf79052a7
RAEnomentsoa <arakotomalala46@gmail.com>
2025-12-08 04:40:50 +0300

    sprint 6 bis
    test

--- test-app/out/helloController.java
+++ test-app/out/helloController.java
@@ -2,6 +2,7 @@ package app.controllers;
 
 import core.annotation.Controller;
 import core.annotation.Route;
+import core.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -12,7 +13,9 @@ public class helloController {
     }
 
     @Route("/hello/test")
-    public String hello(int id) {
+    public String hello(@RequestParam("x") int id) {
+        // url testana hoe mande ex:
+        // http://localhost:8080/framework_test/hello/test?id=11 afaka atao koa x=11
         return "<h1>Hello Controller</h1>"
                 + "<p>ID: " + id + "</p>";
     }
