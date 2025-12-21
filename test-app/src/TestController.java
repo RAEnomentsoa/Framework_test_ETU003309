db739367c5bd456100755ab49a24948d8b6712a9
RAEnomentsoa <arakotomalala46@gmail.com>
2025-12-21 23:18:28 +0300

    fix mistakenly uploded sprint7
    mistakenly uploaded sprint  7

--- test-app/src/TestController.java
+++ test-app/src/TestController.java
@@ -7,7 +7,7 @@ import core.ModelView;
 @Controller
 public class TestController {
 
-    @Route("/test0")
+    @Route("/test")
     public ModelView test() {
         // test method returning a view
         ModelView mv = new ModelView("testView.jsp");
@@ -18,36 +18,9 @@ public class TestController {
 
     }
 
-    @Route("/test0/hello")
+    @Route("/test/hello")
     public String hello() {
         return "<h1>Hello Controller</h1>";
     }
 
-    // sprint 7 test
-    @Route(value = "/test1/sprint", method = "GET")
-    public ModelView getTest() {
-        ModelView mv = new ModelView("test.jsp");
-        mv.addItem("message", "GET request OK");
-        mv.addItem("method", "GET");
-        mv.addItem("id", "-");
-        return mv;
-    }
-
-    @Route(value = "/test1", method = "POST")
-    public ModelView postTest() {
-        ModelView mv = new ModelView("test.jsp");
-        mv.addItem("message", "POST request OK");
-        mv.addItem("method", "POST");
-        mv.addItem("id", "-");
-        return mv;
-    }
-
-    @Route(value = "/test1/{id}", method = "GET")
-    public ModelView getById(int id) {
-        ModelView mv = new ModelView("test.jsp");
-        mv.addItem("message", "GET with path variable");
-        mv.addItem("method", "GET");
-        mv.addItem("id", id);
-        return mv;
-    }
 }
